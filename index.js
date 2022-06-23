3eeb9db5e257aaa40bd5b952891eda3d4d146b36
stephen <stevedogbey@gmail.com>
2022-06-23 09:53:10 +0000

    filter lap completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, nameSearch){
+    return drivers.filter(nameOfDriver => nameOfDriver.toLowerCase() == nameSearch.toLowerCase())
+    }
+
+    
+function fuzzyMatch(drivers, nameSearch){
+    return drivers.filter(nameOfDriver => nameOfDriver.startsWith(nameSearch) )
+    }
+
+    
+function matchName(drivers, nameSearch){
+    return drivers.filter(nameOfDriver => nameOfDriver.name === nameSearch)
+    }
+
